aa71f64e04f85e0c240ce6fcde60d7a58ae72815
Dehbia <dehbia.djebarrii@gmail.com>
2021-04-27 18:24:35 +0200

    server

--- project java/src/Server.java
+++ project java/src/Server.java
@@ -12,5 +12,35 @@ import java.util.concurrent.Executors;
 // Server class
 public class Server
 {
+    private static  ExecutorService pool = Executors.newFixedThreadPool(4);
+    private static ArrayList<ClientHandler> clients = new ArrayList<>();
+    public static void main(String[] args) throws IOException
+    {
+        // le serveur ecoute le port 9900
+        ServerSocket ss = new ServerSocket(9090);
+        // boucle infini attente de la requete d'un client
+        while (true)
+        {
+            Socket s = null; //  vide le socket pour unj nouveau client
+            try
+            {
+                // attend qu'un  client se connecte
+                s = ss.accept();
+                System.out.println("un nouveau client est connecté : " + s);
+
+                System.out.println("donnne un nouveau thread au client");
+                // creation d'un nouveau thread
+                ClientHandler clientThread = new ClientHandler(s);
+                clients.add(clientThread);
+                // execute la methode run contenu dans clienThread
+                pool.execute(clientThread);
+
+            }
+            catch (Exception e){
+                s.close(); // fermetrure du serveu
+                e.printStackTrace(); // retrace l'erreur
+            }
+        }
+    }t
 
 }
