50f5d592765b0f39f344179070c9b9d8a1aa356c
widedhajjar <hajjar@et.esiea.fr>
2021-05-02 18:56:47 +0200

    SoundAPi

--- project java/src/soundAPI/OutgoingSoundListener.java
+++ project java/src/soundAPI/OutgoingSoundListener.java
@@ -4,7 +4,7 @@ package soundAPI;
 
 
 
-import javax.sound.midi.spi.SoundbankReader;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import java.io.*;
@@ -14,8 +14,8 @@ import java.net.Socket;
 public class OutgoingSoundListener{
     public static void runAudio(Socket s, String path) throws IOException {
         try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(FileReader)
-            AudioInputStream.close();
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(File)
+            s.close();
 
 
         } catch (Exception e) {
